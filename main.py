c7c916b1c4d13a66d2270bdb82c45fff16772953
Roman Orlovsky <pub@sunneh.ru>
2021-01-25 23:53:29 +0300

    Task 2

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-inputDate = input("Введите дату: ")
-inputTask = input("Введите задачу: ")
+i = 0
+inputDate = []
+inputTask = []
 
-print(inputDate, inputTask)
+while i < 3:
+  i += 1
+  inputDate.append(input("Введите дату: "))
+  inputTask.append(input("Введите задачу: "))
+
+while len(inputDate) > 0:
+  print(inputDate.pop(0), inputTask.pop(0))
