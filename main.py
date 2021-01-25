d8e4485c71445bb7f0d92cae99e4de15e9833e14
Roman Orlovsky <pub@sunneh.ru>
2021-01-25 23:57:12 +0300

    Task 3

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
+taskList = {}
+
 i = 0
-inputDate = []
-inputTask = []
 
 while i < 3:
   i += 1
-  inputDate.append(input("Введите дату: "))
-  inputTask.append(input("Введите задачу: "))
-
-while len(inputDate) > 0:
-  print(inputDate.pop(0), inputTask.pop(0))
+  inputDate = input("Введите дату: ")
+  inputTask = input("Введите задачу: ")
+  taskList[inputDate] = inputTask
