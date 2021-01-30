138a7b6b2c3cdc3b47daa826b1bff344978c22d6
Roman Orlovsky <pub@sunneh.ru>
2021-01-30 13:55:29 +0300

    Task 2

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 HELP = """Доступные команды: 
-help - Напечатать данную справку
-add - Добавить задачу в список
-print - Напечатать все задачи из списка
-exit - завершить программу"""
+  help  - Напечатать данную справку
+  add   - Добавить задачу в список
+  print - Напечатать все задачи из списка
+  exit  - Завершить программу"""
 
 print(HELP)
 
@@ -16,9 +16,11 @@ while True:
   elif command == 'add':
     inputTask = input("Введите задачу: ")
     tasks.append(inputTask)
+    print(f'Задача \'{inputTask}\' добавлена.')
   elif command == 'print':
-    print(tasks)
+    for task in tasks:
+      print(f'- {task}')
   elif command == 'exit':
     break
   else:
-    print('Неизвестная команда')
+    print('(!) Неизвестная команда')
