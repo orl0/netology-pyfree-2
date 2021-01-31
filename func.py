ed32327255358e4546bca009721a2a7b4955a173
Roman Orlovsky <pub@sunneh.ru>
2021-01-31 22:19:10 +0300

    Task update

--- func.py
+++ func.py
@@ -1,9 +1,8 @@
 def count_letter(words, letter):
   count = 0
   for word in words:
-    for l in word:
-      if l == letter:
-        count += 1
+    if letter in word:
+      count += 1
   return count
 
 words = ['python', 'c++', 'c', 'scala', 'java']
